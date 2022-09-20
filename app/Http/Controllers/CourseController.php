aaef36b37edb246ea93c77dd720a04a20d26c590
Jack Kazanjyan <Jack4ac22@icloud.com>
2022-09-20 09:57:59 +0200

    course controler a good edition to be updated later. for you giuy to be able to see

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -21,7 +21,63 @@ class CourseController extends Controller
      */
     public function index()
     {
-        return response()->json(Course::all());
+        #DB::table('tags')
+        ## ->join('course_tag', 'tags.id', '=', 'course_tag.tag_id')
+        # ->join('courses', 'courses.id', '=', 'course_tag.course_id')
+        # ->select('')
+        $tags = DB::select(DB::raw("SELECT courses.id AS course_id, tags.id AS tag_id, tags.tag_title
+            FROM tags
+            LEFT JOIN course_tag ON tags.id=course_tag.tag_id
+            LEFT JOIN courses ON courses.id=course_tag.course_id"));
+        $tags_by_course = (object)[];
+        foreach ($tags as $entry) {
+            $course_id = $entry->{'course_id'};
+            $tag = array(
+                "tag_id" => $entry->{'tag_id'},
+                "tag" => $entry->{'tag_title'}
+            );
+            if (!isset($tags_by_course->{$course_id})) {
+                $tags_by_course->{$course_id} = array();
+            }
+            $tags_by_course->{$course_id}[] = $tag;
+        }
+        $languages = DB::select(DB::raw("SELECT courses.id AS course_id, languages.id AS language_id, languages.lan_title
+            FROM languages
+            LEFT JOIN language_course ON languages.id=language_course.language_id
+            LEFT JOIN courses ON courses.id=language_course.course_id"));
+        $languages_by_course = (object)[];
+        foreach ($languages as $entry) {
+            $course_id = $entry->{'course_id'};
+            $language = array(
+                "lan_id" => $entry->{'language_id'},
+                "lan_title" => $entry->{'lan_title'},
+            );
+            if (!isset($languages_by_course->{$course_id})) {
+                $languages_by_course->{$course_id} = array();
+            }
+            $languages_by_course->{$course_id}[] = $language;
+        }
+
+        $courses = DB::select(DB::raw("SELECT courses.id, courses.cou_title 
+            FROM courses"));
+        foreach ($courses as $course) {
+            $course_id = $course->{'id'};
+            $course->{'languages'} = array();
+            if (isset($languages_by_course->{$course_id})) {
+                $course->{'languages'} = $languages_by_course->{$course_id};
+            }
+            $course->{'tags'} = array();
+            if (isset($tags_by_course->{$course_id})) {
+                $course->{'tags'} = $tags_by_course->{$course_id};
+            }
+        }
+        return response()->json($courses);
+
+#SELECT courses.id, languages.lan_title
+    #FROM courses
+    #    JOIN language_course ON language_course.course_id=courses.id
+    #    JOIN languages ON languages.id=language_course.language_id
+    #    return response()->json(Course::all());
     }
 
     /**
