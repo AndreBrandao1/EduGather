3d18ff9720a1f0236615b5c983262af05d369e5f
Jack Kazanjyan <Jack4ac22@icloud.com>
2022-09-20 13:55:15 +0200

    bathoom break

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -94,25 +94,59 @@ class LanguageController extends Controller
     public function get_all()
     {
 
-        $courses_by_lan = (object)[];
+        $tags = DB::select(DB::raw(
+            "SELECT courses.id AS course_id, tags.id AS tag_id, tags.tag_title, users.id As user_id, users.first_name, users.last_name
+        FROM tags
+        LEFT JOIN course_tag ON tags.id=course_tag.tag_id
+        LEFT JOIN courses ON courses.id=course_tag.course_id
+        LEFT JOIN users On courses.user_id = users.id"
+        ));
+        $tags_by_course = (object)[];
+        foreach ($tags as $entry) {
+            $course_id = $entry->{'course_id'};
+            $tag = array(
+                "tag_id" => $entry->{'tag_id'},
+                "tag_title" => $entry->{'tag_title'}
+            );
+            if (!isset($tags_by_course->{$course_id})) {
+                $tags_by_course->{$course_id} = array();
+            }
+            $tags_by_course->{$course_id}[] = $tag;
+        }
+
+
+
+        //get the courses then sorte them according to the languages
         $courses = DB::select(DB::raw(
-            "SELECT L.id AS lan_id, C.id AS cou_id, C.cou_title, C.cou_description, C.cou_logo,categories.id,categories.cat_title, categories.cat_logo, U.id AS user_id, U.first_name, U.last_name
+            "SELECT L.id AS lan_id, C.id AS cou_id, C.cou_title, C.cou_description, C.cou_logo,categories.id AS cat_id,categories.cat_title, categories.cat_logo, U.id AS user_id, U.first_name, U.last_name
             From courses AS C
                    LEFT JOIN language_course AS LC ON LC.course_id = C.id
                 LEFT JOIN languages AS L ON LC.language_id = L.id
                 LEFT JOIN categories ON C.cat_id = categories.id
                 LEFT JOIN users AS U ON U.id = C.user_id"
         ));
+
+
+        $courses_by_lan = (object)[];
         foreach ($courses as $entry) {
-            $course_id = $entry->{'course_id'};
+            $lan_id = $entry->{'cou_id'};
             $course = array(
-                "tag_id" => $entry->{'tag_id'},
-                "tag-title" => $entry->{'tag_title'}
+                'cou_id' => $entry->{'cou_id'},
+                'cou_title' => $entry->{'cou_title'},
+                'cou_description' => $entry->{'cou_description'},
+                'cou_logo' => $entry->{'cou_logo'},
+                'cat_id' => $entry->{'cat_id'},
+                'cat_title' => $entry->{'cat_title'},
+                'cat_logo' => $entry->{'cat_logo'},
+                'user_id' => $entry->{'user_id'},
+                'user_first_name' => $entry->{'first_name'},
+                'user_last_name' => $entry->{'last_name'},
+                'tags' => $tags_by_course->{$course_id}
             );
-            if (!isset($courses_by_lan->{$course_id})) {
-                $courses_by_lan->{$course_id} = array();
+            if (!isset($courses_by_lan->{$lan_id})) {
+                $courses_by_lan->{$lan_id} = array();
             }
-            $courses_by_lan->{$course_id}[] = $course;
+            $courses_by_lan->{$lan_id}[] = $course;
         }
 
 
@@ -123,65 +157,10 @@ class LanguageController extends Controller
             LEFT JOIN language_course AS LC ON LC.language_id = languages.id
             "
         ));
+        foreach ($languages as $lan) {
+            $lan_id = $lan->{'id'};
+            $lan->{'courses'} = $courses_by_lan;
+        }
         return response()->json($languages);
-    }
-
-
-
-    public function saving()
-    {
-
-        $tags = DB::select(DB::raw(
-            "SELECT courses.id AS course_id, tags.id AS tag_id, tags.tag_title, users.id As user_id, users.first_name, users.last_name
-    FROM tags
-    LEFT JOIN course_tag ON tags.id=course_tag.tag_id
-    LEFT JOIN courses ON courses.id=course_tag.course_id
-    LEFT JOIN users On courses.user_id = users.id"
-        ));
-        $tags_by_course = (object)[];
-        foreach ($tags as $entry) {
-            $course_id = $entry->{'course_id'};
-            $tag = array(
-                "tag_id" => $entry->{'tag_id'},
-                "tag-title" => $entry->{'tag_title'}
-            );
-            if (!isset($tags_by_course->{$course_id})) {
-                $tags_by_course->{$course_id} = array();
-            }
-            $tags_by_course->{$course_id}[] = $tag;
-        }
-        $languages = DB::select(DB::raw("SELECT courses.id AS course_id, languages.id AS language_id, languages.lan_title
-        FROM languages
-        LEFT JOIN language_course ON languages.id=language_course.language_id
-        LEFT JOIN courses ON courses.id=language_course.course_id"));
-        $languages_by_course = (object)[];
-        foreach ($languages as $entry) {
-            $course_id = $entry->{'course_id'};
-            $language = array(
-                "lan_id" => $entry->{'language_id'},
-                "lan_title" => $entry->{'lan_title'},
-            );
-            if (!isset($languages_by_course->{$course_id})) {
-                $languages_by_course->{$course_id} = array();
-            }
-            $languages_by_course->{$course_id}[] = $language;
-        }
-
-
-        $courses = DB::select(DB::raw("SELECT courses.id, cou_logo, cou_statue, users.id AS user_id, users.first_name, users.last_name, categories.id AS cat_id, categories.cat_title 
-        FROM courses 
-        LEFT JOIN users ON courses.user_id = users.id
-        LEFT JOIN categories ON courses.cat_id = categories.id"));
-        foreach ($courses as $course) {
-            $course_id = $course->{'id'};
-            $course->{'languages'} = array();
-            if (isset($languages_by_course->{$course_id})) {
-                $course->{'languages'} = $languages_by_course->{$course_id};
-            }
-            $course->{'tags'} = array();
-            if (isset($tags_by_course->{$course_id})) {
-                $course->{'tags'} = $tags_by_course->{$course_id};
-            }
-        }
     }
 }
