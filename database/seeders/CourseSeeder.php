53d87605979be906f777662f53bdb78513e062be
Jack Kazanjyan <Jack4ac22@icloud.com>
2022-09-22 11:49:01 +0200

    udpating the seeders

--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -16,8 +16,21 @@ class CourseSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('courses')->insert([
-            ['cou_title' => 'course 1', 'cou_description' => 'something nice about me', 'cou_logo' => 'https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png', 'user_id' => 1, 'cat_id' => 1], ['cou_title' => 'course 2', 'cou_description' => 'something nice about me', 'cou_logo' => 'https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png', 'user_id' => 2, 'cat_id' => 3], ['cou_title' => 'course 3', 'cou_description' => 'something nice about me', 'cou_logo' => 'https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png', 'user_id' => 2, 'cat_id' => 4], ['cou_title' => 'course 4', 'cou_description' => 'something nice about me', 'cou_logo' => 'https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png', 'user_id' => 3, 'cat_id' => 2], ['cou_title' => 'course 5', 'cou_description' => 'something nice about me', 'cou_logo' => 'https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png', 'user_id' => 4, 'cat_id' => 2], ['cou_title' => 'course 6', 'cou_description' => 'something nice about me', 'cou_logo' => 'https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png', 'user_id' => 2, 'cat_id' => 1], ['cou_title' => 'course 7', 'cou_description' => 'something nice about me', 'cou_logo' => 'https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png', 'user_id' => 2, 'cat_id' => 2],
+        DB::table('courses')->insert([['cou_title' => 'course 1', 'cou_description' => 'Course 1 description', 'cou_logo' => 'https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png', 'user_id' => 1, 'cat_id' => 1],
+            ['cou_title' => 'course 2', 'cou_description' => 'Course 2 description', 'cou_logo' => 'https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png', 'user_id' => 2, 'cat_id' => 1],
+            ['cou_title' => 'course 3', 'cou_description' => 'Course 3 description', 'cou_logo' => 'https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png', 'user_id' => 3, 'cat_id' => 1],
+            ['cou_title' => 'course 4', 'cou_description' => 'Course 4 description', 'cou_logo' => 'https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png', 'user_id' => 2, 'cat_id' => 1],
+            ['cou_title' => 'course 5', 'cou_description' => 'Course 5 description', 'cou_logo' => 'https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png', 'user_id' => 5, 'cat_id' => 1],
+            ['cou_title' => 'course 6', 'cou_description' => 'Course 6 description', 'cou_logo' => 'https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png', 'user_id' => 6, 'cat_id' => 1],
+            ['cou_title' => 'course 7', 'cou_description' => 'Course 7 description', 'cou_logo' => 'https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png', 'user_id' => 3, 'cat_id' => 1],
+            ['cou_title' => 'course 8', 'cou_description' => 'Course 8 description', 'cou_logo' => 'https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png', 'user_id' => 6, 'cat_id' => 1],
+            ['cou_title' => 'course 9', 'cou_description' => 'Course 9 description', 'cou_logo' => 'https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png', 'user_id' => 2, 'cat_id' => 1],
+            ['cou_title' => 'course 10', 'cou_description' => 'Course 10 description', 'cou_logo' => 'https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png', 'user_id' => 2, 'cat_id' => 1],
+            ['cou_title' => 'course 11', 'cou_description' => 'Course 11 description', 'cou_logo' => 'https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png', 'user_id' => 3, 'cat_id' => 1],
+            ['cou_title' => 'course 12', 'cou_description' => 'Course 12 description', 'cou_logo' => 'https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png', 'user_id' => 4, 'cat_id' => 1],
+            ['cou_title' => 'course 13', 'cou_description' => 'Course 13 description', 'cou_logo' => 'https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png', 'user_id' => 5, 'cat_id' => 1],
+            ['cou_title' => 'course 14', 'cou_description' => 'Course 14 description', 'cou_logo' => 'https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png', 'user_id' => 2, 'cat_id' => 1],
+            ['cou_title' => 'course 15', 'cou_description' => 'Course 15 description', 'cou_logo' => 'https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png', 'user_id' => 3, 'cat_id' => 1],
 
 
         ]);
--- database/seeders/CourseTagSeeder.php
+++ database/seeders/CourseTagSeeder.php
@@ -17,13 +17,39 @@ class CourseTagSeeder extends Seeder
     public function run()
     {
         DB::table('course_tag')->insert([
-            ['course_id' => 1, 'tag_id' => 1],
-            ['course_id' => 1, 'tag_id' => 2],
-            ['course_id' => 1, 'tag_id' => 3],
-            ['course_id' => 1, 'tag_id' => 4],
-            ['course_id' => 2, 'tag_id' => 3],
-            ['course_id' => 2, 'tag_id' => 6],
-            ['course_id' => 2, 'tag_id' => 7]
+            ['course_id' => 1, 'tag_id' => 1],['course_id' => 1, 'tag_id' => 2], ['course_id' => 1, 'tag_id' => 3], ['course_id' => 2, 'tag_id' => 2],
+            ['course_id' => 2, 'tag_id' => 1],
+            ['course_id' => 2, 'tag_id' => 4],
+            ['course_id' => 3, 'tag_id' => 3],
+            ['course_id' => 3, 'tag_id' => 2],
+            ['course_id' => 4, 'tag_id' => 1],
+            ['course_id' => 4, 'tag_id' => 2],
+            ['course_id' => 4, 'tag_id' => 4],
+            ['course_id' => 5, 'tag_id' => 5],
+            ['course_id' => 5, 'tag_id' => 6],
+            ['course_id' => 6, 'tag_id' => 6],
+            ['course_id' => 6, 'tag_id' => 7],
+            ['course_id' => 7, 'tag_id' => 8],
+            ['course_id' => 7, 'tag_id' => 7],
+            ['course_id' => 8, 'tag_id' => 8],
+            ['course_id' => 8, 'tag_id' => 7],
+            ['course_id' => 8, 'tag_id' => 5],
+            ['course_id' => 9, 'tag_id' => 9],
+            ['course_id' => 9, 'tag_id' => 10],
+            ['course_id' => 10, 'tag_id' => 10],
+            ['course_id' => 11, 'tag_id' => 12],
+            ['course_id' => 11, 'tag_id' => 13],
+            ['course_id' => 11, 'tag_id' => 14],
+            ['course_id' => 12, 'tag_id' => 13],
+            ['course_id' => 12, 'tag_id' => 14],
+            ['course_id' => 13, 'tag_id' => 14],
+            ['course_id' => 13, 'tag_id' => 11],
+            ['course_id' => 14, 'tag_id' => 15],
+            ['course_id' => 14, 'tag_id' => 16],
+            ['course_id' => 15, 'tag_id' => 16],
+            ['course_id' => 15, 'tag_id' => 17],
+            
+
         ]);
     }
 }
