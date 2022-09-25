a0ed9bf6b8b1e6aa2ff23c8d1d650e06e4a73e9d
Jack Kazanjyan <kazanjian.jack.z@gmail.com>
2022-09-25 18:33:06 +0200

    editing some seeders and making the single course api

--- database/factories/LanguagecourseFactory.php
+++ database/factories/LanguagecourseFactory.php
@@ -2,27 +2,23 @@
 
 namespace Database\Factories;
 
-use App\Models\CourseTag;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
  */
-class CoursetagFactory extends Factory
+class LanguagecourseFactory extends Factory
 {
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    protected $model = CourseTag::class;
     public function definition()
     {
-
         return [
+            'language_id' => fake()->numberBetween(1, 7),
             'course_id' => fake()->numberBetween(1, 65),
-            'tag_id' => fake()->numberBetween(1, 17),
         ];
-
     }
 }
